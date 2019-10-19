300f8c3a36e9db58bf46495ebf70533b00cad15d
KaranbirSingh7 <karanbirsingh1161@gmail.com>
2019-10-19 16:38:13 -0400

    test

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} on: ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} on ${PORT}`)
 );
