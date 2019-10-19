928034357a0475c1af4d65806b71c0276c93711b
KaranbirSingh7 <karanbirsingh1161@gmail.com>
2019-10-19 16:53:42 -0400

    Add: routes

---
+++ routes/bootcamps.js
@@ -0,0 +1,15 @@
+// /api/v1/bootcamps/:id
+
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {});
+
+router.post('/:id', (req, res) => {});
+
+router.patch('/:id', (req, res) => {});
+
+router.delete('/:id', (req, res) => {});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const bootcamps = require('./routes/bootcamps');
 
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// MOUNT ROUTES
+app.use('/api/v1/bootcamps', bootcamps);
 
 app.listen(
   PORT,
