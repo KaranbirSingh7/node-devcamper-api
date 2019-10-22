5fda3643f2faae564f4e9358659aa09a1dbffbb0
KaranbirSingh7 <karanbirsingh1161@gmail.com>
2019-10-22 00:50:02 -0400

    Add: db config

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
+// DEPS
 const express = require('express');
 const dotenv = require('dotenv');
-const bootcamps = require('./routes/bootcamps');
 const morgan = require('morgan');
 
+// LOCAL IMPORTS
+const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
+
 dotenv.config({ path: './config/config.env' });
+
+// Connect to DB
+connectDB();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -17,7 +25,14 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/v1/bootcamps', bootcamps);
 
 // START server
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} on ${PORT}`)
 );
+
+// Handle unhandled rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message} `);
+  // Close server with exit
+  server.close(() => process.exit(1));
+});
